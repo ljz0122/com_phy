0fb7ee173452f169ac0a9f95a00eb02304ef87e6
Eric Li <ericljz@hotmail.com>
2020-09-28 21:07:48 +0800

    Remove unused code

--- 1_Pi/custom_math.h
+++ 1_Pi/custom_math.h
@@ -9,7 +9,3 @@
 #endif
 
 void fact(mpz_t result,const int num);
-
-//void mpz_pow(mpz_t result,const mpz_t base,const mpz_t exp);
-
-//void mpz_si_pow(mpz_t result,const long int base,const mpz_t exp);
--- 1_Pi/pi_calc.c
+++ 1_Pi/pi_calc.c
@@ -15,9 +15,8 @@ long double pi_lei_calc(long double pi,int lim){
     for(i = 0;fabsl((quater_pi - quater_pi_before)*4)>=exp;i++){
         quater_pi_before=quater_pi;
         quater_pi=quater_pi+powl((-1),i)*1/(2*i+1);
-        //printf("Pi=%.10Lf\n",quater_pi*4);
     }
-    printf("num=%ld ",i);
+    printf("num=%ld ",i-1);
     pi = 4*quater_pi;
     return pi;
 }
@@ -28,11 +27,9 @@ long double pi_euler_calc(long double pi,int lim){
     unsigned long int i;
     for(i = 1;fabsl(sqrtl(1/powl(i,2)*6))>=exp;i++){
         euler_pi=euler_pi+1/powl(i,2);
-        //printf("Pi=%.10Lf\n",sqrtl(euler_pi*6));
     }
-    printf("num=%ld ",i);
+    printf("num=%ld ",i-1);
     pi = sqrtl(euler_pi*6);
-    //printf("Pi=%.10Lf\n",pi);
     return pi;
 }
 
@@ -69,12 +66,11 @@ void pi_raman_calc(mpfr_t pi,int lim){
         mpz_mul_ui(down,down,(99*99));
         mpz_cdiv_q(item,up,down);
         mpz_add(pi_1,pi_1,item);
-        printf("%ld\n",i);
     }
     mpfr_init2(multi_f,(4*lim+256));
     mpfr_set_z(multi_f,multi,MPFR_RNDN);
     mpfr_div_z(pi,multi_f,pi_1,MPFR_RNDN);
-    printf("num=%ld ",i);
+    printf("num=%ld ",i-1);
     mpfr_clear(multi_f);
     mpz_clears(multi,exp,NULL);
     mpz_clear(pi_1);
@@ -82,21 +78,3 @@ void pi_raman_calc(mpfr_t pi,int lim){
     mpz_clears(item,up,down,NULL);
     mpz_clears(up_1,up_2,down_1,down_2,NULL);
 }
-
-/*int main(int argc,char *argv[]){
-    int N = 100;
-    //int method = atoi(argv[1]);
-    mpfr_t pi;
-    mpfr_init2(pi,4*N+256);
-    pi_raman_calc(pi,N);
-    //long double pi_ld;
-    //pi = pi_euler_calc(pi,N);
-    //printf("Pi=%.10Lf\n",pi_ld);
-    //mpfr_init_set_ld(pi,pi_ld,MPFR_RNDN);
-    printf("Pi=");
-    mpfr_out_str(stdout,10,N+1,pi,MPFR_RNDN);
-    printf("\n");
-    //mpfr_printf("Pi=%.10Rf\n",pi);
-    mpfr_clear(pi);
-    return 0;
-}*/
